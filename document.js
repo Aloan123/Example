34fe4e645ded3eaeaaf859c8a1132f71960041ca
Laon123 <49540606+Aloan123@users.noreply.github.com>
2023-11-17 21:08:39 -0500

    Update document.js

--- document.js
+++ document.js
@@ -1,4 +1,4 @@
-let variblae = 10;
+let variable = 10;
 
 console.log("variable es: " + 10);
 
@@ -6,4 +6,8 @@ let arr = [1, 2, 3, 4, 5];
 
 for (let i = 0; i < 2; i++) {
     console.log(i);
+}
+
+if (arr[0] == 1) {
+    variable = 8;
 }
